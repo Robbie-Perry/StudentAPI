7fddb21a2d1a52c78fc17af27b713c75b804ff7f
Robbie Perry <robbieperry22@gmail.com>
2018-02-05 18:51:07 -0800

    Initial

---
+++ StudentAPI/Data/StudentSeeder.cs
@@ -0,0 +1,46 @@
+﻿using StudentAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentAPI.Data
+{
+    public class StudentSeeder
+    {
+        public static List<Student> getStudents()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student
+                {
+                    StudentId = "A00111111",
+                    FirstName = "John",
+                    LastName = "Archer",
+                    Program = "CIT"
+                },
+                new Student
+                {
+                    StudentId = "A00222222",
+                    FirstName = "Jane",
+                    LastName = "Baker",
+                    Program = "CST"
+                },
+                new Student
+                {
+                    StudentId = "A00333333",
+                    FirstName = "Bill",
+                    LastName = "Carter",
+                    Program = "BTECH"
+                },new Student
+                {
+                    StudentId = "A00444444",
+                    FirstName = "Judy",
+                    LastName = "Fisher",
+                    Program = "Nursing"
+                }
+            };
+            return students;
+        }
+    }
+}
